5efef04ba85cd93f781f490c0280590e67622331
Ricardo Márquez Turiello <ricardo.marquez@cab.cnea.gov.ar>
2017-11-03 15:36:29 -0300

    Creation of the matrix class.
    Testing speed before the implementation in CUDA.

--- src/DCEbiomarkers.cpp
+++ src/DCEbiomarkers.cpp
@@ -1,198 +1,187 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
-#include <vector>
 #include <time.h>
+#include "matrix.h"
 #include "FFT_CODE/fft.h"
 #include "FFT_CODE/complex.h"
 
 #define NX 64
 #define MAXIT 1000
-
-/* class matrix not implemented yet */
-template <typename T>
-class matrix{
-	int ndata;
-	int dim,*sizes;
-	T 	*data;
-};
+#define NPARAMS 3
 
 template <typename T> void writeToBinary(const char *filename, T *data, int ndata, int *sizes, int dim);
 template <typename T> void readFromBinary(const char *filename, T **data, int *ndata, int **sizes, int *dim);
 template <typename T> T norm(T *vec, int n);
-void solveNLLSQ(float *J, float *dCt, float *dB, int ndata, int nparams);
-void solverGauss(float *A,float *b, float *x, int n);
+void solveNLLSQ(float *J, float *dCt, float *dB, int npoints, int nparams);
+void gaussSolver(float *A,float *b, float *x, int n);
 
 int main(void){
-  int ndata;
-  int *Ct_sizes,Ct_dim,Ct_ndata;
-  int *t_sizes,t_dim,t_ndata;
-  int *AIF_sizes,AIF_dim,AIF_ndata=1;
-  float *Ct_data,*Ct_aux,*AIF_data,*t_data;
+	matrix<float> t("t.dat");							/* Read t */
+  int nt = t.getNumData();							/* Size of temporal dimension */
+  float *time = t.getData();						/* Time data */
+	matrix<float> AIF("AIF.dat");					/* Read AIF */
+	matrix<float> Ct("C_t.dat"); 					/* Read C_t but still needs a data reordering */
+	int nsd = Ct.getDim()-1;							/* Number of spatial dimensions */
+	int *mriSizes = Ct.getSizes();				/* MRI Sizes */ 
+	int nVoxels = 1;
+	for(int i=0;i<nsd;i++)
+  	nVoxels *= mriSizes[i];							/* Number of MRI voxels */
+  float *Ct_aux = new float[nVoxels*nt];/* Here will be reordered C_t */
 
-  /* Leo AIF */
-  readFromBinary("AIF.dat",&AIF_data,&AIF_ndata,&AIF_sizes,&AIF_dim);
-  
-  /* Leo t */
-  readFromBinary("t.dat",&t_data,&t_ndata,&t_sizes,&t_dim);
-  float difft = (t_data[t_ndata-1]-t_data[0])/(t_ndata-1);
+	/* Average temporary step */
+  float difft = (time[nt-1]-time[0])/(nt-1);	
 
-  /* Leo C_t */
-  readFromBinary("C_t.dat",&Ct_aux,&Ct_ndata,&Ct_sizes,&Ct_dim);
-  Ct_data = new float[Ct_ndata];   /* Cambio el orden en que se guarda los datos en Ct; El tiempo pasa a ser el primer indice */
-  int aux_step = Ct_sizes[0]*Ct_sizes[1]*Ct_sizes[2];
-  for(int i=0;i<Ct_sizes[0];i++){
-    for(int j=0;j<Ct_sizes[1];j++){
-      for(int k=0;k<Ct_sizes[2];k++){
-        int aux_offset = k*Ct_sizes[0]*Ct_sizes[1] + j*Ct_sizes[0] + i;
-        int data_offset = aux_offset*t_ndata;
-        for(int l=0;l<Ct_sizes[3];l++)
-          Ct_data[data_offset+l] = Ct_aux[aux_offset + l*aux_step];
+  /* Reordering of C_t data. Data along time of each voxel will be continuous */
+  int aux_step = nVoxels;
+  for(int i=0;i<mriSizes[0];i++){
+    for(int j=0;j<mriSizes[1];j++){
+      for(int k=0;k<mriSizes[2];k++){
+        int aux_offset = k*mriSizes[0]*mriSizes[1] + j*mriSizes[0] + i;
+        int data_offset = aux_offset*nt;
+        Ct.getDataValues(aux_offset,aux_step,nt,Ct_aux,data_offset,1);
+//        for(int l=0;l<mriSizes[3];l++)
+//          Ct.setDataValue(data_offset+l,Ct_aux.getDataValue(aux_offset + l*aux_step));
       }
     }
   }
+	Ct.setData(Ct_aux);	/* Copy the C_t reordered to Ct */
+	delete[] Ct_aux;
   
-  /* Prueba con un pixel cualquiera */
+  /* Test with any voxel */
 /*  int px=248,py=275,pz=11;
-  for(int i=0;i<Ct_sizes[3];i++){
+  for(int i=0;i<mriSizes[3];i++){
     int idx= i*512*512*24 + (pz-1)*512*512 + (py-1)*512 + (px-1);
-    printf("%f %f %f\n",t_data[i],AIF_data[i],Ct_data[idx]);
+    printf("%f %f %f\n",time[i],AIF.data[i],Ct.data[idx]);
   }  */
 
-  /* Alloca memoria para vp, ktrans, kep y otros aux */
-  ndata = Ct_sizes[0]*Ct_sizes[1]*Ct_sizes[2];
-  float *vp = new float[ndata];
-  float *ktrans = new float[ndata];
-  float *kep = new float[ndata];
-  float *J = new float[3*t_ndata];
-  float *integral = new float[t_ndata];
-  float *dintegral = new float[t_ndata];
-  float *Ct = new float[t_ndata];
-  float *Ct_k = new float[t_ndata];
-  float *dCt = new float[t_ndata];
-  float *converged = new float[ndata];
-  complex *Cp = new complex[NX];
-  complex *fftCp = new complex[NX];
-  complex *Exp = new complex[NX];
-  complex *fftExp = new complex[NX];
-  complex *convol = new complex[NX];
+  /* Alloc memory for vp, ktrans, kep y other variables */
+	matrix<float> vp(3,mriSizes,nVoxels);					/* Vp matrix */
+	matrix<float> ktrans(3,mriSizes,nVoxels);			/* Ktrans matrix */
+	matrix<float> kep(3,mriSizes,nVoxels);				/* Kep matrix */
+	matrix<float> converged(3,mriSizes,nVoxels);	/* Matrix of convergence of the NLLSQ method */
+  float *J = new float[NPARAMS*nt];					/* Jacobian for the NLLSQ method */
+  float *Ct_i = new float[nt];  						/* Ct data from 1 voxel */
+  float *Ct_n = new float[nt];							/* Ct numerical approximation */
+  float *dCt = new float[nt];								/* Diference between Ct_i and Ct_n */
+  float *Cp_re = new float[NX];							/* AIF */
+  complex *Cp = new complex[NX];						/* AIF (complex)*/
+  complex *fftCp = new complex[NX];					/* fft(AIF) */
+  complex *Exp = new complex[NX];						/* Exponential function */
+  complex *fftExp = new complex[NX];				/* fft(Exp) or fft(Exp.*t) */
+  complex *convol = new complex[NX];				/* ifft(fftCp.*fftExp) */
+  float *integral = new float[nt];					/* Truncated real part of convol using fft(Exp) */
+  float *dintegral = new float[nt];					/* Truncated real part of convol using fft(Exp.*t) */
 
-  Cp = new complex[NX];
-  for(int i=0;i<NX;i++){      /* Inicializo a 0 todas los elementos */
-    Cp[i] = 0.0;  
+  for(int i=0;i<NX;i++){      /* Initialize to 0 all elements */
+    Cp[i] = Cp_re[i] = 0.0;  
     Exp[i] = 0.0;  
   }
-  for(int i=0;i<t_ndata;i++)
-    Cp[i] = AIF_data[i];      /* Copio AIF en la parte real de Cp */
-  fftCp = new complex[NX];
-  CFFT::Forward(Cp,fftCp,NX);  /* FFT de Cp */
+  for(int i=0;i<nt;i++)    		/* Copy AIF to the real part of Cp */
+    Cp[i] = Cp_re[i] = AIF.getDataValue(i); 
+  CFFT::Forward(Cp,fftCp,NX); /* FFT of Cp */
 /*  for(int i=0;i<NX;i++)  
-    printf("%f %f %f\n",Cp[i].re(),fftCp[i].re(),fftCp[i].im());  */
-  
+    printf("%f %f %f %f\n",Cp[i].re(),Cp_re[i],fftCp[i].re(),fftCp[i].im()); */
+
   size_t t_i=clock();
   printf("Starting calculation...\n");  
-	for(int i=0;i<Ct_sizes[0];i++){
+	for(int i=0;i<mriSizes[0];i++){
  	//for(int i=213;i<247;i++){
-    int maxit_step = 0;
-    for(int j=0;j<Ct_sizes[1];j++){
+    int maxit_step = 0;								/* Maximum iteration number needed for convergence in a step 'i' */
+    for(int j=0;j<mriSizes[1];j++){
     //for(int j=239;j<278;j++){
-      for(int k=0;k<Ct_sizes[2];k++){
+      for(int k=0;k<mriSizes[2];k++){
       //for(int k=10;k<11;k++){
 				//printf("Voxel(%d,%d,%d)\n",i,j,k);
-        /* Parámetros iniciales */ 
-        int param_offset = k*Ct_sizes[0]*Ct_sizes[1] + j*Ct_sizes[0] + i;
-        int data_offset = param_offset*t_ndata;
+        /* Initial parameters */
+        int param_offset = k*mriSizes[0]*mriSizes[1] + j*mriSizes[0] + i;
+        int data_offset = param_offset*nt;
         float vp0=0.1,ktrans0=0.3/60,kep0=0.3/60;
-        converged[param_offset] = 0;
-        for(int l=0;l<Ct_sizes[3];l++){
-          Ct[l] = Ct_data[data_offset + l];
-          Exp[l] = exp(-kep0*t_data[l]);
-					//printf("%f %f %f %f\n",Ct[l],Exp[l].re());
-        }
-        CFFT::Forward(Exp,fftExp,NX);
+        Ct.getDataValues(data_offset,1,mriSizes[3],Ct_i,0,1);		/* Ct data of voxel(i,j,k) */
+        for(int l=0;l<mriSizes[3];l++)
+          Exp[l] = exp(-kep0*time[l]);				/* Exponential function */
+					//printf("%f %f %f %f\n",Ct_i[l],Exp[l].re());
+
+        CFFT::Forward(Exp,fftExp,NX);						/* fft(Exp) */
         for(int l=0;l<NX;l++)
-          fftExp[l] *= fftCp[l];
-        CFFT::Inverse(fftExp,convol,NX);
-        for(int l=0;l<Ct_sizes[3];l++){
-          integral[l] = convol[l].re()*difft;
-          Ct_k[l] = vp0*AIF_data[l] + ktrans0*integral[l];
-          dCt[l] = Ct[l] - Ct_k[l];
+          fftExp[l] *= fftCp[l];								/* fftExp.*fftCp */
+        CFFT::Inverse(fftExp,convol,NX);				/* ifft(fftCp.*fftExp) */
+        for(int l=0;l<mriSizes[3];l++){
+          integral[l] = convol[l].re()*difft;		/* Truncated real part of convol using fft(Exp) */
+          Ct_n[l] = vp0*Cp_re[l] + ktrans0*integral[l];	/* Ct numerical approximation */
+          dCt[l] = Ct_i[l] - Ct_n[l];						/* Diference between Ct_i and Ct_n */
         }
-        float r_old = norm(dCt,t_ndata);
-        
-        /* Cuadrados mínimos no-lineal */
+        float r_old = norm(dCt,nt);
+  
+        /* Non-Liniear Least Square Method */
   			int its = 0;
-				int nparams = 3;
-        float dB[3]={1e10,1e10,1e10},beta[3],beta_min[3];
+        float dB[NPARAMS]={1e10,1e10,1e10};
+				float beta[NPARAMS]={vp0,ktrans0,kep0};  		/* SHOULD CHANGE IF NPARAMS CHANGE */
+				float beta_min[NPARAMS]={vp0,ktrans0,kep0};	/* SHOULD CHANGE IF NPARAMS CHANGE */
         float dr=1e10;
         float r_min=1e10,r_new;
-        while( its<MAXIT && (norm(dB,3)>1e-6||dr>1e-3) && kep0>1e-9 ){        
+        while( its<MAXIT && (norm(dB,NPARAMS)>1e-6||dr>1e-3) && kep0>1e-9 ){        
           its++;
-          for(int l=0;l<Ct_sizes[3];l++)
-            Exp[l] *= t_data[l];
+          for(int l=0;l<mriSizes[3];l++)
+            Exp[l] *= time[l];
           CFFT::Forward(Exp,fftExp,NX);
           for(int l=0;l<NX;l++)
             fftExp[l] *= fftCp[l];
           CFFT::Inverse(fftExp,convol,NX);
-          for(int l=0;l<Ct_sizes[3];l++){
+          for(int l=0;l<mriSizes[3];l++){
             dintegral[l] = convol[l].re()*difft;
-            J[l] = AIF_data[l];    
-            J[t_ndata+l] = integral[l];
-            J[2*t_ndata+l] = -ktrans0*dintegral[l];
+            J[l] = Cp_re[l];    
+            J[nt+l] = integral[l];
+            J[2*nt+l] = -ktrans0*dintegral[l];
 						//if(its==1)
-							//printf("%f %f %f\n",J[l],J[t_ndata+l],J[2*t_ndata+l]);
+							//printf("%f %f %f %f\n",J[l],J[nt+l],J[2*nt+l],dCt[l]);
           }
-          solveNLLSQ(J,dCt,dB,t_ndata,nparams);
-          if((vp0+dB[0])<0)
-            dB[0] = -vp0*3/4;      
-          if((ktrans0+dB[1])<0)
-            dB[1] = -ktrans0*3/4;      
-          if((kep0+dB[2])<0)
-            dB[2] = -kep0*3/4;      
-          vp0 += dB[0];
-          ktrans0 += dB[1];
-          kep0 += dB[2];
-          beta[0] = vp0;
-          beta[1] = ktrans0;
-          beta[2] = kep0;
-					//printf("%f,%f,%f\n",beta[0],beta[1],beta[2]);
-          for(int l=0;l<Ct_sizes[3];l++)
-            Exp[l] = exp(-kep0*t_data[l]);
+          solveNLLSQ(J,dCt,dB,nt,NPARAMS);
+					for(int i=0;i<NPARAMS;i++){
+          	if((beta[i]+dB[i])<0)
+            	dB[i] = -3.0/4.0*beta[i];      
+						beta[i] += dB[i];
+					} 
+          vp0 = beta[0];
+          ktrans0 = beta[1];
+          kep0 = beta[2];
+	
+          for(int l=0;l<mriSizes[3];l++)
+            Exp[l] = exp(-kep0*time[l]);
           CFFT::Forward(Exp,fftExp,NX);
           for(int l=0;l<NX;l++)
             fftExp[l] *= fftCp[l];
           CFFT::Inverse(fftExp,convol,NX);
-          for(int l=0;l<Ct_sizes[3];l++){
+          for(int l=0;l<mriSizes[3];l++){
             integral[l] = convol[l].re()*difft;
-            Ct_k[l] = vp0*AIF_data[l] + ktrans0*integral[l];
-            dCt[l] = Ct[l] - Ct_k[l];
+            Ct_n[l] = vp0*Cp_re[l] + ktrans0*integral[l];
+            dCt[l] = Ct_i[l] - Ct_n[l];
           }
-          r_new = norm(dCt,t_ndata);
+          r_new = norm(dCt,nt);
           if(r_new<r_min){
             r_min=r_new;
-            beta_min[0] = beta[0];
-            beta_min[1] = beta[1];
-            beta_min[2] = beta[2];
+						for(int i=0;i<NPARAMS;i++)
+            	beta_min[i] = beta[i];
           }  
           dr = fabs(r_new-r_old)/r_new;
-					//printf("%f %f %f %f %f %f\n",dr,r_new,norm(dB,nparams),beta[0],beta[1],beta[2]);
+					//printf("%f %f %f %f %f %f\n",dr,r_new,norm(dB,NPARAMS),beta[0],beta[1],beta[2]);
 					//getchar();
           r_old = r_new;
         }
-        if(its!=MAXIT)
-          converged[param_offset] = 1;
-        vp[param_offset] = beta_min[0];
-        ktrans[param_offset] = beta_min[1];
-        kep[param_offset] = beta_min[2];
+        if(its!=MAXIT)	
+					converged.setDataValue(param_offset,1);
+        vp.setDataValue(param_offset,beta_min[0]);
+        ktrans.setDataValue(param_offset,beta_min[1]);
+        kep.setDataValue(param_offset,beta_min[2]);
         if(its>maxit_step && its<MAXIT)
           maxit_step = its;
       }
     }  
 		if((i+1)%50==0){
-	    writeToBinary("vp.dat",vp,ndata,Ct_sizes,3);
-  	  writeToBinary("ktrans.dat",ktrans,ndata,Ct_sizes,3);
-    	writeToBinary("kep.dat",kep,ndata,Ct_sizes,3);
-    	writeToBinary("converged.dat",converged,ndata,Ct_sizes,3);
+	    vp.writeToBinary("vp.dat");
+  	  ktrans.writeToBinary("ktrans.dat");
+    	kep.writeToBinary("kep.dat");
+    	converged.writeToBinary("converged.dat");
 		}
     printf("Voxel(%d,:,:) -> ",i+1);
     printf("max.its = %4d -> ",maxit_step);
@@ -211,20 +200,20 @@ T norm(T *vec, int n){
   return sqrt(norm);
 }
 
-void solveNLLSQ(float *J, float *dCt, float *dB, int ndata, int nparams){
+void solveNLLSQ(float *J, float *dCt, float *dB, int npoints, int nparams){
 	int J_offset,JT_offset;
   float JTxJ[nparams][nparams], JTxdCt[nparams], aux;
   for(int i=0;i<nparams;i++){
-    JT_offset = i*ndata;
+    JT_offset = i*npoints;
     for(int j=0;j<nparams;j++){
       aux=0;
-      J_offset = j*ndata;
-      for(int k=0;k<ndata;k++)
+      J_offset = j*npoints;
+      for(int k=0;k<npoints;k++)
         aux += J[JT_offset+k] * J[J_offset+k];
       JTxJ[i][j] = aux;
     }
     aux = 0;
-    for(int k=0;k<ndata;k++)
+    for(int k=0;k<npoints;k++)
       aux += J[JT_offset+k] * dCt[k];
     JTxdCt[i] = aux;
   }
@@ -235,12 +224,12 @@ void solveNLLSQ(float *J, float *dCt, float *dB, int ndata, int nparams){
 			printf(" %f ",JTxJ[i][j]);
 		printf(" | | %f |\n",JTxdCt[i]);
 	}	*/
-  solverGauss(&JTxJ[0][0],JTxdCt,dB,nparams);
+  gaussSolver(&JTxJ[0][0],JTxdCt,dB,nparams);
 /*  for(int i=0;i<nparams;i++)
 		printf("| %f |\n",dB[i]);	*/
 }
 
-void solverGauss(float *A,float *b, float *x, int n){
+void gaussSolver(float *A,float *b, float *x, int n){
   int cont=0,pivot;
   float m[n][n],rhs[n],aux,tol=1e-9;
   
@@ -326,36 +315,3 @@ void readFromBinary(const char *filename, T **data, int *ndata, int **sizes, int
   fread(*data,sizeof(T),*ndata,fid);  
   fclose(fid);
 }
-
-/* Replaced by a template */
-/*
-void writeToBinary(const char *filename, float *data, int ndata, int *sizes, int dim);
-void writeToBinary(const char *filename, float *data, int ndata, int *sizes, int dim){
-  FILE *fid;
-  fid = fopen(filename,"wb"); 
-  assert(fid);
-  fwrite(&dim,sizeof(int),1,fid);
-  fwrite(sizes,sizeof(int),dim,fid);
-  fwrite(data,sizeof(float),ndata,fid);  
-  fclose(fid);
-}	*/
-
-/* Replaced by a template */
-/*
-void readFromBinary(const char *filename, float **data, int *ndata, int **sizes, int *dim);
-void readFromBinary(const char *filename, float **data, int *ndata, int **sizes, int *dim){
-  FILE *fid;
-  fid = fopen(filename,"rb"); 
-  assert(fid);
-  fread(dim,sizeof(int),1,fid);
-  *sizes = new int[*dim];
-  assert(*sizes);
-  fread(*sizes,sizeof(int),*dim,fid);
-  *ndata = 1;
-  for(int i=0;i<*dim;i++)
-    *ndata*=(*sizes)[i];
-  *data = new float[*ndata];
-  assert(*data);
-  fread(*data,sizeof(float),*ndata,fid);  
-  fclose(fid);
-}	*/
---
+++ src/matrix.h
@@ -0,0 +1,124 @@
+/****************/
+/* Class MATRIX */
+/****************/
+
+template <typename T>
+class matrix{
+		int dim;
+		int *sz;
+		int nd;
+		T 	*data;
+
+	public:
+		/* Constructors */
+		matrix(): dim(0),sz(NULL),nd(0),data(NULL){};
+		matrix(const char *filename){ this->readFromBinary(filename); };
+		matrix(int ndim, int *sizes, int ndata): matrix(ndim,sizes,ndata,NULL){};
+		matrix(int ndim, int *sizes, int ndata, T *d): dim(ndim),nd(ndata){
+			sz = new int[ndim];
+			assert(sz);
+			for(int i=0;i<ndim;i++)
+				sz[i] = sizes[i];
+			data = new T[ndata];
+			assert(data);
+			if(d==NULL){
+				for(int i=0;i<ndata;i++)
+					data[i] = 0;
+			}else{
+				for(int i=0;i<ndata;i++)
+					data[i] = d[i];
+			}
+		};
+
+		/* Destructor */
+		~matrix(){ this->free(); };
+
+		/* Mutators */
+		void setData(T *d){	
+			if(data==NULL)	data = new T[nd];
+			assert(data);
+			for(int i=0;i<nd;i++)
+				data[i]=d[i];
+		};
+		inline void setDataValue(int idx, T val){	
+			if(idx<nd) 	data[idx]=val; 
+			else				assert(idx<nd);
+		};
+		/* Set nel values in data[idx_m+i*m] from val[idx_n+i*n] */
+		inline void setDataValues(int idx_m, int m, int nel, T *val, int idx_n, int n){	
+			for(int i=0;i<nel;i++)	setDataValue(idx_m+i*m,val[idx_n+i*n]);
+		};
+
+		/* Accessors */
+		inline int getDim(){ return dim; };
+		inline int getNumData(){ return nd; };
+		inline int getSize(int idx){ 
+			if(idx>=dim)	assert(idx<dim);
+			return sz[idx];
+		}; 
+		inline int* getSizes(){ 
+			int *sizes = new int[dim];
+			for(int i=0;i<dim;i++)
+				sizes[i] = sz[i];
+			return sizes;
+		};
+		inline T getDataValue(int idx){	
+			if(idx>=nd) 	assert(idx<nd);
+			return data[idx];
+		};
+		/* Get nel values from data[idx_m+i*m] to val[idx_n+i*n] */
+		inline void getDataValues(int idx_m, int m, int nel, T *val, int idx_n, int n){	
+			for(int i=0;i<nel;i++)	val[idx_n+i*n] = getDataValue(idx_m+i*m); 
+		};
+		inline T* getData(){
+			T *d = new T[nd];
+			assert(d);
+			for(int i=0;i<nd;i++)
+				d[i]=data[i];
+			return d;
+		};
+		inline T* getDataPointer(){	return data; };
+
+		/* I/O */
+		/* Read from a binary file */
+		void readFromBinary(const char *filename);
+		/* Write to a binary file */
+		void writeToBinary(const char *filename);
+
+		/* Free memory of sz and data */
+		inline void free(){
+			delete[] sz;
+			delete[] data;
+		};
+};
+
+/* Escritura a un archivo binario */
+template <typename T>
+void matrix<T>::writeToBinary(const char *filename){
+  FILE *fid;
+  fid = fopen(filename,"wb"); 
+  assert(fid);
+  fwrite(&dim,sizeof(int),1,fid);
+  fwrite(sz,sizeof(int),dim,fid);
+  fwrite(data,sizeof(T),nd,fid);  
+  fclose(fid);
+}
+
+/* Lectura desde un archivo binario */
+template <typename T>
+void matrix<T>::readFromBinary(const char *filename){
+  FILE *fid;
+  fid = fopen(filename,"rb"); 
+  assert(fid);
+  fread(&dim,sizeof(int),1,fid);
+  sz = new int[dim];
+  assert(sz);
+  fread(sz,sizeof(int),dim,fid);
+  nd = 1;
+  for(int i=0;i<dim;i++)
+    nd *= sz[i];
+  data = new T[nd];
+  assert(data);
+  fread(data,sizeof(T),nd,fid);  
+  fclose(fid);
+}
